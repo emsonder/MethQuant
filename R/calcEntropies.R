79266a8c6e248b4ccd9d88554009af93ec4a473e
esonder <esonder@student.ethz.ch>
2021-03-17 15:09:53 +0100

    Renaming and deletion of unused methods

--- R/calcEntropies.R
+++ R/calcEntropies.R
@@ -8,70 +8,7 @@ library(Rcpp)
 first <- data.table::first
 source("./R/binSequence.R")
 sourceCpp("./C++/entropies.cpp")
-
-#'@title widthEntropy
-#'@description Calculates Sample Entropies along methylation patterns
-#'of genomic subsequences of single cells (width axis).
-#'Sample Entropy: Richman and Moorman, 2000
-#'https://journals.physiology.org/doi/full/10.1152/ajpheart.2000.278.6.h2039
-#'Binary Entropy: Croll, 2013
-#'https://arxiv.org/ftp/arxiv/papers/1305/1305.0954.pdf
-#'@param dataTable Either wide data.table of methylation rates.
-#'dimensions: genome coordinates (pos, chr) x cells. 
-#'@param cellIds unique Identifiers of single cells
-#'@param binByCell Cell-specific binning
-#'@param nCpGsBin Number of CpGs per Bin
-#'@param templateDim Size of methylation templates (embedding dimension for 
-#'sample entropy)
-#'@param aggregateOn Column of table defining the genomic subsequences to 
-#'calculate the entropy scores on. 
-#'@return data.table with Entropies per genomic subsequence and cell
-widthEntropy <- function(metTable, cellIds, 
-                         binByCell=T, nCpGsBin=128,
-                         templateDim=2,
-                         aggregateOn="bin", scaleFactors=c(2,4,8)){
-  # Loop proved to be faster than conversion to long
-  widthEntropies <- list()
-  for(cellId in cellIds)
-  {
-    if(binByCell){
-      columns <- c("pos", "chr", cellId)}
-    else{
-      columns <- c("pos", "chr", cellId, aggregateOn)}
-  
-    # Retrieve met values for one cell
-    cellTable <- metTable[complete.cases(metTable[,..cellId]),..columns]
-    setnames(cellTable, cellId, "rate")
-    
-    # Make sure table is ordered
-    setorder(cellTable, chr, pos)
-    
-    nCpGs <- nrow(cellTable)
-    if(binByCell) cellTable[, (aggregateOn):=fixedBinning(nCpGs, nCpGsBin)] 
-    
-    # Possible methylation rates: 0, 1, 0.5 (allele-specific methylation)
-    cellTable[,rate:=round_any(rate, 0.5)]
-    #cellTable[,rate:=fifelse(rate==1 | rate==0, rate, 0.5)]
-    
-    # Calculate Sample Entropy along width axis & aggregate
-    widthEntropies[[cellId]] <- cellTable[,.(width_SampleEn=sampleEn(rate, templateDim, 0),
-                                             width_BiEn=biEn(rate, tresBin=F),
-                                             width_MsEn_sc1=msEn(rate, scaleFactors[1]), # Make it generic
-                                             width_MsEn_sc2=msEn(rate, scaleFactors[2]),
-                                             width_MsEn_sc3=msEn(rate, scaleFactors[3]),
-                                             nCpGs_width=.N,
-                                             bin_start = min(pos),
-                                             bin_end = max(pos),
-                                             methylation_level_cell=mean(rate),
-                                             mean_dis_CpGs=(last(pos)-first(pos))/.N,
-                                             median_pos=as.integer(median(pos))),
-                                            by=c(aggregateOn, "chr")]
-  }
-  
-  widthEntropies <- rbindlist(widthEntropies,  idcol="cell_id")
-  
-  return(widthEntropies)
-}
+sourceCpp("./C++/mhl.cpp")
 
 .getTemplates <- function(cellTable, m){
   
@@ -115,7 +52,7 @@ widthEntropy <- function(metTable, cellIds,
 #'@return data.table with Entropies per genomic subsequence and cell
 widthKeepSampEn <- function(metTable, 
                             cellIds, 
-                            nTempsBin=100,
+                            nTempsBin=32,
                             templateDim=2,
                             tiledBinning=T){
   
@@ -228,7 +165,62 @@ widthKeepSampEn <- function(metTable,
   return(widthEntropies)
 }
 
-#'@title heightEntropy
+#'@title widthMHL
+#'@description Calculates MHL score along methylation patterns
+#'of genomic subsequences of single cells (width axis). Instead of reads it is
+#'calculated along strips of CpGs not intersected by missing values in the data.
+#'MHL score: Guo et al., 2017
+#'https://pubmed.ncbi.nlm.nih.gov/28263317/
+#'@param metTable Either wide data.table of methylation rates.
+#'dimensions: genome coordinates (pos, chr) x cells. 
+#'@param cellIds unique Identifiers of single cells
+#'@return data.table with MHL scores per genomic subsequence and cell.
+widthMHL <- function(metTable, cellIds)
+{
+  
+  # Loop proved to be faster than conversion to long
+  widthMHLScores <- list()
+  for(cellId in cellIds)
+  {
+    columns <- c("pos", "chr", cellId)
+    
+    cellTable <- metTable[,..columns]
+    setnames(cellTable, cellId, "rate")
+    setorder(cellTable, chr, pos)
+    
+    # Rounding 
+    cellTable[,rate:=round_any(rate, 0.5)]
+    
+    fill <- tidyr::fill
+    # Find all adjacent Templates of arbitrary length
+    cellTable[,isEnd:=fifelse(is.na(shift(rate,n=1,type="lead")) & !is.na(rate), TRUE, FALSE)]
+    cellTable[,tempId:=fifelse(isEnd, pos, as.integer(NA))]
+    cellTable[,tempId:=nafill(tempId, type="nocb")]
+    
+    cellTable <- cellTable[!is.na(rate),]
+    cellTable[,N_adj:=.N, by=tempId]
+    cellTable <- cellTable[N_adj>=2,]
+    
+    
+    cellTable[,width_mhl:=mhl(rate), by=tempId]
+    
+    widthMHLScores[[cellId]] <- cellTable[,.(width_mhl=mhl(rate),
+                                             nCpGs_width=.N,
+                                             bin_start = min(pos),
+                                             bin_end = max(pos),
+                                             met_level_width=mean(rate),
+                                             mean_dis_CpGs=(last(pos)-first(pos))/.N,
+                                             median_pos=as.integer(median(pos))),
+                                          by=c("tempId", "chr")] # by chr
+  }
+  
+  widthMHLScores <- rbindlist(widthMHLScores,  idcol="cell_id")
+  
+  return(widthMHLScores)
+}
+
+
+#'@title heightShannonEn
 #'@description Calculates Shannon Entropies of methylation levels across  
 #'different cells (height axis). Aggregated on genomic subsequences
 #'Shannon Entropy: Shannon, 1948
@@ -239,7 +231,7 @@ widthKeepSampEn <- function(metTable,
 #'@param aggregateOn Column of metTable defining the genomic subsequences to 
 #'aggregate the entropy scores on. 
 #'@return data.table with Shannon Entropies for the genomic subsequences
-heightEntropy <- function(metTable, cellIds, aggregateOn="bin"){
+heightShannonEn <- function(metTable, cellIds, aggregateOn="bin"){
   
   # Reshape to long 
   metTableLong <- melt(metTable, id.vars=c("pos", "chr", aggregateOn),
@@ -261,9 +253,64 @@ heightEntropy <- function(metTable, cellIds, aggregateOn="bin"){
   
   heightEntropies <- metTableLong[,.(height_entropy=mean(height_entropy, na.rm=T),
                                      mean_nCpGs_height=mean(nCpGs, na.rm=T),
-                                     methylation_level_bulk=mean(rate, na.rm=T),
+                                     met_level_height=mean(rate, na.rm=T),
                                      median_pos=as.integer(median(pos))), 
-                                     by=aggregateOn]
+                                  by=aggregateOn]
   
   return(heightEntropies)
+}
+
+#'@title widthShannonEn
+#'@description Calculates Shannon Entropies along methylation patterns
+#'of genomic subsequences of single cells (width axis).
+#'Shannon Entropy: Shannon, 1948
+#'http://people.math.harvard.edu/~ctm/home/text/others/shannon/entropy/entropy.pdf
+#'@param metTable Either wide data.table of methylation rates.
+#'dimensions: genome coordinates (pos, chr) x cells. 
+#'@param cellIds unique Identifiers of single cells
+#'@param nCpGsBin Number of CpGs per Bin
+#'@param aggregateOn Column of table defining the genomic subsequences to 
+#'calculate the entropy scores on. 
+#'@return data.table with entropies per genomic subsequence and cell
+widthShannonEn <- function(metTable, cellIds, 
+                           binByCell=T, nCpGsBin=15,
+                           aggregateOn="bin"){
+  # Loop proved to be faster than conversion to long
+  widthEntropies <- list()
+  for(cellId in cellIds)
+  {
+    if(binByCell){
+      columns <- c("pos", "chr", cellId)}
+    else{
+      columns <- c("pos", "chr", cellId, aggregateOn)}
+    
+    # Retrieve met values for one cell
+    cellTable <- metTable[complete.cases(metTable[,..cellId]),..columns]
+    setnames(cellTable, cellId, "rate")
+    
+    # Make sure table is ordered
+    setorder(cellTable, chr, pos)
+    
+    nCpGs <- nrow(cellTable)
+    if(binByCell) cellTable[, (aggregateOn):=fixedBinning(nCpGs, nCpGsBin)] 
+    
+    # Possible methylation rates: 0, 1, 0.5 (allele-specific methylation)
+    cellTable[,rate:=round_any(rate, 0.5)]
+    cellTable[,methylation_level_width:=mean(rate), by=c(aggregateOn, "chr")]
+    
+    # Calculate Sample Entropy along width axis & aggregate
+    # TODO: Check ordering!r
+    widthEntropies[[cellId]] <- cellTable[,.(met_level_width=mean(rate),
+                                             width_ShannonEn=shannonEnDiscrete(rate),
+                                             nCpGs_width=.N,
+                                             bin_start=min(pos),
+                                             bin_end=max(pos),
+                                             mean_dis_CpGs=(last(pos)-first(pos))/.N,
+                                             median_pos=as.integer(median(pos))),
+                                          by=c(aggregateOn, "chr")]
+  }
+  
+  widthEntropies <- rbindlist(widthEntropies,  idcol="cell_id")
+  
+  return(widthEntropies)
 }
