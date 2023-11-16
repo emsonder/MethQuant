b5d784f63b40b46992aa203acc3947d8a988b4e9
emsonder <esonder@outlook.com>
2023-11-16 10:51:12 +0000

    binning & read modes

--- R/methScores.R
+++ R/methScores.R
@@ -34,19 +34,19 @@
 #' @export
 methScores <- function(data, # can be GRanges, or bsseq, or data.table / data.frame
                        score=c("sampEn", "shannonEn"), # one of the implemented scores ? but how to ensure correctness ?
-                       axis=c("w", "h"),
+                       axis=c("w", "h", "reads"),
                        posCol="pos",
                        seqCol="chr",
-                       binMode=c("costum", "tiled", "fixed"),
+                       binMode=c("custom", "tiled", "fixed"),
                        regions=NULL,
                        startCol="start",
                        endCol="end",
-                       binSize=1000,
+                       binSize=1000
                        ...){
 
     # check input arguments
-    if(!(score %in% c("sampEn", "shannonEn"))){
-      stop("Please choose one of the following scores: sampEn, shannonEn") # maybe add a constants file
+    if(!(score %in% c("sampEn", "shannonEn", "metLevel", "pdr", "fdrp", "trProb", "smc"))){
+      stop("Please choose one of the following scores: sampEn, shannonEn, metLevel, pdr, fdrp") # maybe add a constants file
     }
 
     # Retrieve the methylation matrix
@@ -64,118 +64,164 @@ methScores <- function(data, # can be GRanges, or bsseq, or data.table / data.fr
     }
     else if(is(data, "data.frame") || is(data, "data.table")){
       methData <- as.data.table(data)
-      }
+    }
+    else if(is(data, 'sparseMatrix') || is.character(data))
+    {
+        print("If .bam-file or a CpGs x Reads matrix is provided.
+              Scores are calculated based on reads (arg axis=reads expected)")
+
+        if(axis!="reads") stop("axis=reads expected if data is provided as
+                                 a path to a .bam-file or sparse CpGs x Read matrix")
+
+        #if(!file.exists(data)){
+        #    stop("A path to a .bam-file or sparse CpGs x Read matrix is expected as
+        #          data if one of the read-based scores
+        #          (pdr, fdrp, qfdrp, epipolymorphism, epialleleEntropy) is choosen")}
+    }
     else{
       stop("Data class must be one of the following:
-            BSseq, GRanges, data.frame or data.table (preferred)")
+            BSseq, GRanges, data.frame, data.table (preferred) or path to .bam-file")
     }
 
-    cellIds <- setdiff(colnames(methData), c(seqCol, posCol))
-    methData[,c(startCol, endCol):=get(posCol)]
-
-    # Binning
-    if(binMode=="costum")
+    if(axis!="reads")
     {
-      regionsDt <- as.data.table(regions)
-      setnames(regionsDt, c("seqnames", "start", "end"), c(seqCol, startCol, endCol),
-               skip_absent=TRUE)
-      regionsDt[,bin:=paste0("(", chr, ",", start, ",", end, "]")]
-      regionsDt$bin <- factor(regionsDt$bin, levels=unique(regionsDt$bin))
-      setkeyv(regionsDt, c(seqCol, startCol, endCol))
+      cellIds <- setdiff(colnames(methData), c(seqCol, posCol))
+      methData[,c(startCol, endCol):=get(posCol)]
 
-      methData <- foverlaps(methData,
-                            regionsDt[,c(seqCol, startCol, endCol, "bin"),
-                                    with=FALSE],
-                            by.x=c(seqCol, startCol, endCol),
-                            by.y=c(seqCol, startCol, endCol),
-                            type="within",
-                            nomatch=NULL)
-    }
-    else if(binMode=="tiled")
-    {
-      #methData[, bin:=cut(pos, seq(min(get(posCol)),
-      #                             max(get(posCol))+binSize, binSize),
-      #                    include.lowest=TRUE), by=seqCol]
-      methData[,maxPos:=max(get(posCol)), by=seqCol]
-      methData[, bin:=.binning(max(get(posCol)), binSize,
-                              get(posCol), get(seqCol),
-                              mode="tiled"),
-                 by=seqCol]
-      methData$maxPos <- NULL
-    }
-    else if(binMode=="fixed")
-    {
-      methData[, bin:=.binning(.N, binSize,
-                              get(posCol), get(seqCol),
-                              mode="fixed"),
-                 by=seqCol]
-    }
-    else if(binMode=="moving")
-    {
-      stop("Moving bin mode not yet implemented")
-    }
+      # Binning
+      if(binMode=="custom")
+      {
+        regionsDt <- as.data.table(regions)
+        setnames(regionsDt, c("seqnames", "start", "end"), c(seqCol, startCol, endCol),
+                 skip_absent=TRUE)
+        regionsDt[,bin:=paste0(chr, "_", start, "_", end)]
+        #regionsDt[,bin:=paste0("(", chr, ",", start, ",", end, "]")]
+        regionsDt$bin <- factor(regionsDt$bin, levels=unique(regionsDt$bin))
+        setkeyv(regionsDt, c(seqCol, startCol, endCol))
 
-    methData <- methData[,c(seqCol, posCol, cellIds, "bin"), with=FALSE]
-    #TODO: Ensure ordering
+        methData <- foverlaps(methData,
+                              regionsDt[,c(seqCol, startCol, endCol, "bin"),
+                                      with=FALSE],
+                              by.x=c(seqCol, startCol, endCol),
+                              by.y=c(seqCol, startCol, endCol),
+                              type="within",
+                              nomatch=NULL)
+      }
+      else if(binMode=="tiled")
+      {
+        methData[,maxPos:=max(get(posCol)), by=seqCol]
+        methData[, bin:=.binning(max(get(posCol)), binSize,
+                                get(posCol), get(seqCol),
+                                mode="tiled"),
+                   by=seqCol]
+        methData$maxPos <- NULL
+      }
+      else if(binMode=="fixed")
+      {
+        methData[, bin:=.binning(.N, binSize,
+                                get(posCol), get(seqCol),
+                                mode="fixed"),
+                   by=seqCol]
+      }
+      else if(binMode=="moving")
+      {
+        stop("Moving bin mode not yet implemented")
+      }
 
-    scoreFun <- get(score) # here use keyword args
-    if(axis=="h")
-    {
-      methData <- melt(methData, id.vars=c(seqCol, "bin", posCol))
-      setnames(methData, c("variable", "value"), c("cell_id", "met_level"))
+      methData <- methData[,c(seqCol, posCol, cellIds, "bin"), with=FALSE]
+      #TODO: Ensure ordering
 
-      # could also do specifically not with posCol
-      if(score=="sampEn") block <- c(seqCol, "bin", posCol) else block <- c(seqCol, "bin")
-      scores <- scoreFun(methData, cols=c("met_level"),
-                         block=block, ...)
-
-      #scores <- shannonEn(methData, cols=c("met_level"), block=c(seqCol, "bin", posCol))
-      setnames(scores, c(paste(score, "met_level", sep="_")),
-                       c(paste(score, axis, sep="_")))
-    }
-    else if(axis=="w")
-    {
-      scores <- scoreFun(methData, cols=cellIds,
-                         block=c(seqCol, "bin"), ...)
-     # scores <- shannonEn(methData, cols=cellIds, block=c(seqCol, "bin"))
-      scores <- melt(scores, id.vars=c(seqCol, "bin"),
-                     value.name=paste(score, axis, sep="_"))
-
-      scores[,sample_id:=tstrsplit(variable, split="_", keep=2)]
-      scores$variable <- NULL
-    }
-    else
-    {
-      stop("Axis needs to be either w (width, intra-cell) or h (height, inter-cell)")
-    }
-
-    # add as a slot if bsseq or granges
-    if(is(data, "BSseq"))
-    {
+      scoreFun <- get(score) # here use keyword args
       if(axis=="h")
       {
-        if(binMode=="costum")
-        {
-          data <- subsetByOverlaps(data, regions)
-        }
+        methData <- melt(methData, id.vars=c(seqCol, "bin", posCol))
+        setnames(methData, c("variable", "value"), c("cell_id", "met_level"))
 
-        metadata(data) <- append(metadata(data), list("entropyScores"=scores))
+        # could also do specifically not with posCol
+        if(score=="sampEn") block <- c(seqCol, "bin", posCol) else block <- c(seqCol, "bin")
+        scores <- scoreFun(methData, cols=c("met_level"),
+                           block=block, ...)
+
+        #scores <- shannonEn(methData, cols=c("met_level"), block=c(seqCol, "bin", posCol))
+        setnames(scores, c(paste(score, "met_level", sep=":")),
+                         c(paste(score, axis, sep=":")))
       }
       else if(axis=="w")
       {
-        scoreCol <- setdiff(names(scores), c(seqCol, "bin", "sample_id"))
-        scoresWide <- dcast(scores, bin~sample_id, value.var=scoreCol)
-        colNames <- paste(score, scoresWide$bin, sep="_")
-        scoresWide$bin <- NULL
+        scores <- scoreFun(methData, cols=cellIds,
+                           block=c(seqCol, "bin"), ...)
 
-        scoresWide <- DataFrame(t(scoresWide))
-        colnames(scoresWide) <- colNames
+        scores <- melt(scores, id.vars=c(seqCol, "bin"),
+                       value.name=paste(score, axis, sep=":"))
 
-        colData(data) <- cbind(colData(data), scoresWide)
+        scores[,sample_id:=tstrsplit(variable, split=":", keep=2)]
+        scores$variable <- NULL
+      }
+      else
+      {
+        stop("Axis needs to be either w (width, intra-cell) or h (height, inter-cell)")
       }
 
-      return(list(data, scores))
+      # add as a slot if bsseq or granges
+      if(is(data, "BSseq"))
+      {
+        if(axis=="h")
+        {
+          if(binMode=="costum")
+          {
+            data <- subsetByOverlaps(data, regions)
+          }
+
+          metadata(data) <- append(metadata(data), list("entropyScores"=scores))
+        }
+        else if(axis=="w")
+        {
+          scoreCol <- setdiff(names(scores), c(seqCol, "bin", "sample_id"))
+          scores[,(scoreCol):=as.numeric(scoreCol)]
+          scoresWide <- dcast(scores, bin~sample_id, value.var=scoreCol)
+          colNames <- paste(score, scoresWide$bin, sep="_")
+          scoresWide$bin <- NULL
+
+          scoresWide <- DataFrame(t(scoresWide))
+          colnames(scoresWide) <- colNames
+
+          colData(data) <- cbind(colData(data), scoresWide)
+        }
+
+        return(list(data, scores))
+      }
+    }
+    else
+    {
+      scores <- methScoresReads(data, score, regions, ...)
     }
 
     return(scores)
+}
+
+methScoresReads <- function(data, score, regions, seed=43, ...){
+
+  # Check input arguments
+  if(!(score %in% c("pdr", "fdrp", "qfdrp", "epipolymorphism", "epialleleEntropy"))){
+    stop("Please choose one of the following scores: pdr, fdrp, qfdrp, epipolymorphism, epialleleEntropy")
+  }
+
+  if(is.null(regions) & !is(data, 'sparseMatrix'))
+  {
+    stop("Regions need to be provided for read-based scores")
+  }
+
+  if(!is(data, 'sparseMatrix'))
+  {
+    if(!file.exists(data)){
+    stop("A path to a .bam-file or sparse CpGs x Read matrix is expected as
+          data if one of the read-based scores
+          (pdr, fdrp, qfdrp, epipolymorphism, epialleleEntropy) is choosen")}
+  }
+
+  # calculate scores
+  scoreFun <- get(score) # here use keyword args
+  scores <- scoreFun(data, regions, ...)
+
+  return(scores)
 }
